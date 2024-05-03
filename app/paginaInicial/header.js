d456932c7c216bf184946ba0fec54d99f11b361d
Hugo Henrique <imhugooooo@gmail.com>
2024-05-03 20:35:43 -0300

    estruturando HTML pagina inicial

---
+++ app/paginaInicial/header.js
@@ -0,0 +1,23 @@
+
+
+export default function Header(){
+    return(
+        <header>
+
+            <h1> Guilty </h1>
+            <nav>
+                <ul>
+                    <li>Log In</li>
+                    <li>Suport</li>
+                    <li>About us</li>
+                </ul>
+            </nav>
+
+        </header>
+        
+
+
+
+    );
+
+}
