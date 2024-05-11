8f2bdfabb8f5b34a657bd47751b113b9d99c7ae7
Rhuan <153238918+Rhuu4n@users.noreply.github.com>
2024-05-10 22:02:09 -0300

    btn entrar e variavel idSala funcionais

--- app/components/criar-salas.js
+++ app/components/criar-salas.js
@@ -1,19 +1,38 @@
+import { useState } from "react";
+
 export default function Criar_Salas(){
+
+    const [idSala, alteraIdSala] = useState("");
+
+    function alteraInput(event){
+        const value = event.target.value;
+        // Remove todos os caracteres que não são números
+        const newValue = value.replace(/\D/g, '');
+        // Limita o valor a 4 caracteres
+        const limitedValue = newValue.slice(0, 4);
+        // Atualiza o valor do campo
+        event.target.value = limitedValue;
+        alteraIdSala(limitedValue);
+    };
+    
+    function enviarIdSala(event) {
+        event.preventDefault();
+        alteraIdSala();
+        alert(idSala);
+    }
+
     return(
         <div className="boxPartida ">
-
             <button className="btn Centralizar">Criar Partida</button>
+            <p className="lblEntrar">Entre em uma partida</p>
 
-            <p className="lblEntrar">Entre em uma partida:</p>
+            <form onSubmit={(event)=> enviarIdSala(event)}>
 
-            <div className="boxEntrar">
-
-                <input className="txtEntrar"/>
-
-                <button className="btnEntrar">Entrar</button>
-
-            </div>
-
+                <div className="boxEntrar">
+                    <input type="text" className="txtEntrar" onChange={(event)=> alteraInput(event)}/>
+                    <button className="btnEntrar">Entrar</button>
+                </div>
+            </form>
         </div>
     );
 }
