8cd71555e643494354b0f635d5db26af1999ab3c
DANILO CONCEICAO CANDIDO <danilo.ccandido@senacsp.edu.br>
2024-05-10 21:37:31 -0300

    Flexbox painel adm

--- app/paineladm/page.js
+++ app/paineladm/page.js
@@ -1,42 +1,54 @@
+"use client"
+import { useState } from "react";
 import "./paineladm.css"
 export default function PainelAdministrativo(){
-    return(
-        <div id="Tablesadm">
-            <div>
-                <header>
-                    <section className="SeçãoCabeçalho" >
-                    <h1>Painel ADM</h1>
-                    <nav>
-                        <ul>
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">Páginas</a></li>
-                            <li><a href="#">Configurações</a></li>
-                        </ul>
-                    </nav>
-                    </section>
-                </header>
-                <main>
-                    <section className="SeçãoTabelas" >
-                    <h2>Painel de Controle</h2>
+    const [selecaoTabela, alteraSelecaoTabela] = useState("Estatisticas");
 
-                <div >
+    return(
+        <div className="flex-container">
+            <header>
+                <section className="SeçãoCabeçalho" >
+                <h1>Painel ADM</h1>
+                <nav>
+                    <ul>
+                        <li><a href="#">Home</a> <a href="#">Páginas</a> <a href="#">Configurações</a> </li>
+                    </ul>
+                </nav>
+                </section>
+                <br/>
+                <div className="lista-secoes">
+                <h2>Painel de Controle</h2>
+                    <ul>
+                        <li>Estatisticas <button onClick={()=> alteraSelecaoTabela("Estatisticas")} >Acessar</button> </li> 
+                        <li>Ranking <button onClick={()=> alteraSelecaoTabela("Ranking")}>Acessar</button> </li>
+                        <li>Numero de jogadores<button onClick={()=> alteraSelecaoTabela("Numerojogadores")}>Acessar</button> </li>
+                    </ul>
+                </div>
+            </header>
+
+            <main>
+            <section className="Tabelas" >
+                {
+                selecaoTabela == "Estatisticas" &&   
+                <div className="TabelaEstatisticas" >
                 <table border={"true"} >
-                    <caption>Estatisticas</caption>
+                <caption>Estatisticas</caption>
                     <thead>
                         <tr>
                             <th >Numero de partidas</th> <td>20 Partidas</td>
-                            
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                        <th >Total de usuários</th> <td>Usuários: 50</td>
+                            <th >Total de usuários</th> <td>Usuários: 50</td>
                         </tr>
                     </tbody>
                 </table>
                 </div>
-
-                <div>
+                }   
+                {
+                selecaoTabela == "Ranking" &&
+                <div className="TabelaNumerojogadores" >
                 <table border={"true"} >
                     <caption>Ranking</caption>
                     <thead>
@@ -55,13 +67,29 @@ export default function PainelAdministrativo(){
                     </tbody>
                     </table>
                 </div>
-                    </section>
-                </main>
+                }
+                {
+                selecaoTabela == "Numerojogadores" &&
+                <div className="TabelaRanking">
+                <table border={"true"} >
+                    <caption>Numero de jogadores</caption>
+                    <thead>
+                        <tr>
+                            <th >Numero de jogadores totais</th>  <td>3500</td>
+                        </tr>
+                        <tr>
+                            <th >Numero de jogadores online</th> <td>150</td>
+                        </tr>
+                    </thead>
+                </table>
+                </div>
+                }
+            </section>
+            </main>
 
-                <footer>
-                    <a href="https://github.com/Rhuu4n/projeto_web.git" target="blank" > Game Guilty, algo assim</a>
-                </footer>
-            </div>
+            <footer>
+                <a href="https://github.com/Rhuu4n/projeto_web.git" target="blank" > Game Guilty, algo assim</a>
+            </footer>
         </div>
     )
 }
