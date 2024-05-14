645b7ef24f6f7579597401b60f94b55e70c37669
Erick Rosdachimo <erickrosdachimoribeiro@gmail.com>
2024-05-14 19:52:08 -0300

    commitolas

--- app/partida/page.js
+++ app/partida/page.js
@@ -1,9 +1,21 @@
 import HeaderPartida from "./headerPartida";
+import "./partida.css"
 
 const Partida = () => {
     return (
-        <div>
+        <div id="partida">
             <HeaderPartida/>
+            <div className="areaPartida">
+                <div className="topo">
+
+                </div>
+                <div className="meio">
+                
+                </div>
+                <div className="fim">
+                
+                </div>
+            </div>
         </div>
     );
 }
