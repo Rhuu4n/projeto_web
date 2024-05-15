fe41a31bc5545ccd850c06893f07b5387abf9ed9
Rhuan <153238918+Rhuu4n@users.noreply.github.com>
2024-05-14 22:20:26 -0300

    primeiro

--- app/termo/linhas.js
+++ app/termo/linhas.js
@@ -1,8 +1,37 @@
-import Letras from "./letras";
+"use client"
+import { useState } from "react";
+import "./termo.css"
+
 export default function Linha(){
+
+    const [letra, alteraLetra] = useState();
+
+    let campo= 1
+
+    const linha = [
+        <input className="txt1" onChange={(event)=> inserir(event)}></input>,
+        <input className="txt2" onChange={(event)=> inserir(event)}></input>,
+        <input className="txt3" onChange={(event)=> inserir(event)}></input>,
+        <input className="txt4" onChange={(event)=> inserir(event)}></input>,
+        <input className="txt5" onChange={(event)=> inserir(event)}></input>
+    ]
+
+    function inserir(event){
+        const value = event.target.value;
+        // Remove todos os caracteres que não são números
+        const newValue = value.replace(/[^a-zA-Z]/g, '');
+        // Limita o valor a 4 caracteres
+        const limitedValue = newValue.slice(0, 1);
+        // Atualiza o valor do campo
+        event.target.value = limitedValue;
+        alteraLetra(limitedValue);
+        document.querySelector(".txt" + campo).focus()
+        campo++
+    };
+
     return(
-        <div className="linha">
-            <Letras/>
+        <div id="linha">
+            
         </div>
     );
 }
