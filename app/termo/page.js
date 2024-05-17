755e4bc6f5b027a19d427100f71d0d14cb6e0b5f
Rhuan <153238918+Rhuu4n@users.noreply.github.com>
2024-05-16 22:27:03 -0300

    teste

--- app/termo/page.js
+++ app/termo/page.js
@@ -1,17 +1,21 @@
 "use client"
 import "./termo.css"
 import Linha from "./linhas";
+import Link from "next/link";
 
 export default function Termo(){
+
     return(
         <div id="termo">
             <h1 className="lblTermo">Termo</h1>
-            <Linha/>
-            <Linha/>
-            <Linha/>
-            <Linha/>
-            <Linha/>
-            <Linha/>
+
+            <Linha readOnly="enable"/>
+            <Linha readOnly="disable"/>
+            <Linha readOnly="disable"/>
+            <Linha readOnly="disable"/>
+            <Linha readOnly="disable"/>
+            <Linha readOnly="disable"/>
+
         </div>
     );
 }
--- app/termo/teste.js
+++ app/termo/teste.js
@@ -1,51 +1,45 @@
 "use client"
-import { useRef, useState } from "react";
-import "./termo.css"
+import { useState, useRef } from "react";
+import "./termo.css";
 
-export default function Linha(props){
-
+export default function Linha(props) {
     const [letra, alteraLetra] = useState();
-
     const [campo, alteraCampo] = useState(1);
-
     const inputsRef = useRef([]);
 
-    let readOnly = [
-        {
-            enable:"",
-            disable: "disable"
-        }
-        ]
-
-    function inserir(event){
+    function inserir(event) {
         const value = event.target.value;
-        // Remove todos os caracteres que não são números
-        const newValue = value.replace(/[^a-zA-Z]/g, '').toUpperCase();
-        // Limita o valor a 4 caracteres
+        // Remove todos os caracteres que não são letras
+        const newValue = value.replace(/[^a-zA-Z]/g, '');
+        // Limita o valor a 1 caractere
         const limitedValue = newValue.slice(0, 1);
         // Atualiza o valor do campo
         event.target.value = limitedValue;
         alteraLetra(limitedValue);
-        //document.querySelector(".txt" + campo).focus()
+
         if (limitedValue) {
             // Muda o foco para o próximo input
             const nextCampo = campo + 1;
             alteraCampo(nextCampo);
             if (nextCampo > 5) {
-                alteraCampo(0);
+                alteraCampo(1);
             } else {
                 inputsRef.current[nextCampo - 1].focus();
             }
         }
     };
 
-    return(
-        <div id="linha" onSubmit={props.submit}>
+    return (
+        <div id="linha">
             {[1, 2, 3, 4, 5].map((num) => (
-            <input  readOnly={readOnly[0].props.readOnly}
+                <input
+                    key={num}
+                    readOnly={props.disable}
                     className={`txt${num}`}
-                    onChange={(event)=> inserir(event)}
-                    ref={(el) => (inputsRef.current[num - 1] = el)}/>))}
+                    onChange={inserir}
+                    ref={(el) => (inputsRef.current[num - 1] = el)}
+                />
+            ))}
         </div>
     );
 }
