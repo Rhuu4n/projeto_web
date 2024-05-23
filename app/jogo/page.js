18c2b3090ac660425b4c034d6fc41da7a948d793
Érick Rosdachimo Ribeiro <96780514+Erick-Rosdachimo@users.noreply.github.com>
2024-05-23 19:47:29 -0300

    mexendo na partida guilty

--- app/jogo/page.js
+++ app/jogo/page.js
@@ -1,22 +1,33 @@
 "use client"
 
-import Header_Salas from "../components/header-salas"
-import Criar_Salas from "../components/criar-salas"
+import Header_Salas from "./header-salas"
+import Criar_Salas from "./criar-salas"
 import "./salas.css"
 import { useState } from "react"
+import Lobby from "../lobby/lobby"
 
 export default function Jogo(){
 
     const [idSala, alteraIdSala] = useState("");
     const [autenticacao, alteraAutenticacao ] = useState("");
+    const [salaOrLobby, alteraSalaOrLobby] = useState("sala");
 
     return(
 
         <div id="salas">
-            
-                <Header_Salas nome="Jorge"/>
+                {
+                    salaOrLobby == "sala" ? 
+                    <>
+                        <Header_Salas autenticacao={autenticacao} alteraAutenticacao={alteraAutenticacao} nome="Jorge"/> 
+                        <Criar_Salas idSala={idSala} alteraIdSala={alteraIdSala} alteraSalaOrLobby={alteraSalaOrLobby} link="img/logo.png"/>
+                    </> :
 
-                <Criar_Salas link="img/logo.png"/>
+                    <Lobby idSala={idSala}/>
+
+                    
+                }
+                
+
         </div>
     )
 }
