ef189ec543723a781cfd747ef310b04e18588faf
Hugo <imhugooooo@gmail.com>
2024-06-06 17:40:12 -0300

    terminando detalhes home page e fazendo btnVerjogos

--- app/homePage/body.js
+++ app/homePage/body.js
@@ -3,7 +3,6 @@
 
 
 import { useEffect, useRef, useState } from "react"
-import {motion, useScroll, useSpring} from "framer-motion"
 import "./font.css"
 import "./styleBody.css"
 
@@ -12,28 +11,6 @@ import "./styleBody.css"
 
 export default function Body () {
 
-    const items = [
-        {
-        id: 1,
-        title: "Guilty",
-        img: "https://c4.wallpaperflare.com/wallpaper/892/692/922/howl-s-moving-castle-studio-ghibli-fantasy-art-clouds-daylight-hd-wallpaper-thumb.jpg"
-        },
-        {
-        id: 2,
-        title: "Termo",
-        img: "https://c4.wallpaperflare.com/wallpaper/262/774/423/space-stars-nebula-tylercreatesworlds-wallpaper-thumb.jpg"
-        },
-        {
-        id: 3,
-        title: "Em breve",
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6UpMqGGuVEA4rzvlOVec211MdnAj5_ddVS6XTddbKE56tIIrxTsNxaftOOEHlHcZKQw4&usqp=CAU"
-        },
-        {
-        id: 4,
-        title: "Em breve",
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6UpMqGGuVEA4rzvlOVec211MdnAj5_ddVS6XTddbKE56tIIrxTsNxaftOOEHlHcZKQw4&usqp=CAU"
-        }
-    ];
 
 
 
@@ -41,7 +18,14 @@ export default function Body () {
 
         <div id="container-body">
 
+            <div className="contentBody">
 
+                <div className="txtinicial">
+                    <p>Uma plataforma de jogos para voce passar o tempo </p>
+                </div>
+
+            </div>
+            <button className="btnVerjogos">Ver jogos </button>
         </div>
 
     )
