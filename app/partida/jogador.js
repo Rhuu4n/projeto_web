d9bfbbea1d2de85074de1d1aeac0d986700320d0
Erick Rosdachimo <erickrosdachimoribeiro@gmail.com>
2024-06-19 18:31:29 -0300

    reconhece quando e sua vez.

--- app/partida/jogador.js
+++ app/partida/jogador.js
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import './jogador.css'
 
 const Jogador = props => {
@@ -7,7 +9,38 @@ const Jogador = props => {
   const [acao1, alteraAcao1] = useState(0)
   const [acao2, alteraAcao2] = useState(0)
 
-  useEffect(() => {}, [props.podeJogar])
+  const showToastMessage = () => {
+    toast.success('Sua vez!')
+  }
+
+  useEffect(() => {
+    if (props.podeJogarRef === true) {
+      let num1 = Math.floor(Math.random() * 2) + 1
+      let num2 = Math.floor(Math.random() * 2) + 1
+
+      if (num1 === 1) {
+        alteraAcao1(1)
+        alteraCarta1('mineradora.jpg')
+      } else if (num1 === 2) {
+        alteraAcao1(2)
+        alteraCarta1('saqueador.jpg')
+      } else {
+        alert('1 outro numero' + num1)
+      }
+
+      if (num2 === 1) {
+        alteraAcao2(1)
+        alteraCarta2('mineradora.jpg')
+      } else if (num2 === 2) {
+        alteraAcao2(2)
+        alteraCarta2('saqueador.jpg')
+      } else {
+        alert('2 outro numero' + num2)
+      }
+
+      showToastMessage()
+    }
+  }, [props.podeJogarRef])
 
   return (
     <div id="joguin" className={props.position}>
@@ -19,8 +52,8 @@ const Jogador = props => {
         </div>
       </div>
       <div className="box-cartas">
-        <img onclick={() => {}} src={"img/" + carta1} />
-        <img onclick={() => {}} src={"img/" + carta2} />
+        <img onclick={() => {}} src={'img/' + carta1} />
+        <img onclick={() => {}} src={'img/' + carta2} />
       </div>
     </div>
   )
--- app/partida/partida.js
+++ app/partida/partida.js
@@ -3,21 +3,25 @@ import HeaderPartida from './headerPartida'
 import Jogador from './jogador'
 import './partida.css'
 import axios from 'axios'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
-const Partida = props => { 
+const Partida = props => {
   const [mudaCartas, alteraMudaCartas] = useState(false)
   const [podeJogar, alteraPodeJogar] = useState(false)
   const [jogadorAtual, alteraJogadorAtual] = useState(0)
   const [jaRodou, alteraJaRodou] = useState(false)
   const [attribute, setAttribute] = useState(false)
+  const jaRodouRef = useRef(false)
+  const podeJogarRef = useRef(false)
   const initialized = useRef(false)
 
   let nomeJogadores = props.ordemJogadores
   let idJogadores = props.jogadoresIdPartida
 
-
-  async function main(){
+  async function main() {
     const interval = setInterval(async () => {
+      console.log('inicio interval')
       const token = localStorage.getItem('token')
       const response_room = await axios.get(`/api/rooms/${props.idSala}`, {
         headers: {
@@ -28,19 +32,19 @@ const Partida = props => {
 
       let resAtual = response_room.data.jogadorAtual
 
-      if(resAtual == props.ordem && !jaRodou){
+      if (resAtual == props.ordem && !jaRodouRef.current) {
+        console.log('entrou')
         alteraPodeJogar(true)
-        alteraJogadorAtual(resAtual)
         alteraJaRodou(true)
+        jaRodouRef.current = true
+        podeJogarRef.current = true
       }
-
 
       if (attribute) {
         clearInterval(interval) // Para a rotina se o atributo mudar
         console.log('Atributo mudou, rotina parada')
       }
     }, 1000)
-  
   }
 
   useEffect(() => {
@@ -55,24 +59,42 @@ const Partida = props => {
     }
   }, [])
 
+  useEffect(() => {}, [podeJogar])
+  useEffect(() => {}, [jaRodou])
+
   return (
     <div id="partida">
+      <ToastContainer />
       <HeaderPartida />
       <div className="areaPartida">
         <div className="topo">
-          <Jogador nome={nomeJogadores[2]} idPartida={idJogadores[2]}/>
+          <Jogador nome={nomeJogadores[2]} idPartida={idJogadores[2]} />
         </div>
         <div className="meio">
           <div className="esquerda">
-            <Jogador nome={nomeJogadores[1]} idPartida={idJogadores[1]} position="esquerda" />
+            <Jogador
+              nome={nomeJogadores[1]}
+              idPartida={idJogadores[1]}
+              position="esquerda"
+            />
           </div>
           <div className="centro"></div>
           <div className="direita">
-            <Jogador nome={nomeJogadores[3]} idPartida={idJogadores[3]} position="direita" />
+            <Jogador
+              nome={nomeJogadores[3]}
+              idPartida={idJogadores[3]}
+              position="direita"
+            />
           </div>
         </div>
         <div className="fim">
-          <Jogador podeJogar={podeJogar} nome={nomeJogadores[0]} idPartida={idJogadores[0]} position="eu"/>
+          <Jogador
+            podeJogarRef={podeJogarRef.current}
+            podeJogar={podeJogar}
+            nome={nomeJogadores[0]}
+            idPartida={idJogadores[0]}
+            position="eu"
+          />
         </div>
       </div>
     </div>
