330d893e2e6618f71bf5891f2a491547bd5bf1a4
Érick Rosdachimo Ribeiro <96780514+Erick-Rosdachimo@users.noreply.github.com>
2024-06-18 21:38:17 -0300

    inicio partida

--- app/lobby/components/bodyLobby.js
+++ app/lobby/components/bodyLobby.js
@@ -51,30 +51,67 @@ const BodyLobby = props => {
           'jogador4'
         ]
 
-        let jogador1 = response.data[0] == undefined ? 'jogador' : response.data[0].Jogador_ID
-        let jogador2 = response.data[1] == undefined ? 'jogador' : response.data[1].Jogador_ID
-        let jogador3 = response.data[2] == undefined ? 'jogador' : response.data[2].Jogador_ID
-        let jogador4 = response.data[3] == undefined ? 'jogador' : response.data[3].Jogador_ID
+        let idJogadores = [
+          'jogador1',
+          'jogador2',
+          'jogador3',
+          'jogador4'
+        ]
+
+        let jogador1 = response.data[0] == undefined ? 'jogador' : await pegaUser(response.data[0].Jogador_ID)
+        let jogador2 = response.data[1] == undefined ? 'jogador' : await pegaUser(response.data[1].Jogador_ID)
+        let jogador3 = response.data[2] == undefined ? 'jogador' : await pegaUser(response.data[2].Jogador_ID)
+        let jogador4 = response.data[3] == undefined ? 'jogador' : await pegaUser(response.data[3].Jogador_ID)
+
+        let id1 = response.data[0] == undefined ? 'jogador' : response.data[0].id_partida
+        let id2 = response.data[1] == undefined ? 'jogador' : response.data[1].id_partida
+        let id3 = response.data[2] == undefined ? 'jogador' : response.data[2].id_partida
+        let id4 = response.data[3] == undefined ? 'jogador' : response.data[3].id_partida
 
         if (props.numeroJogadores == 1) {
+          nomeJogadores[0] = jogador1 == undefined ? 'jogador' : jogador1
           nomeJogadores[1] = jogador2 == undefined ? 'jogador' : jogador2
           nomeJogadores[2] = jogador3 == undefined ? 'jogador' : jogador3
           nomeJogadores[3] = jogador4 == undefined ? 'jogador' : jogador4
+
+          idJogadores[0] = id1 == undefined ? 'jogador' : id1
+          idJogadores[1] = id2 == undefined ? 'jogador' : id2
+          idJogadores[2] = id3 == undefined ? 'jogador' : id3
+          idJogadores[3] = id4 == undefined ? 'jogador' : id4
         } else if (props.numeroJogadores == 2) {
+          nomeJogadores[0] = jogador2 == undefined ? 'jogador' : jogador2
           nomeJogadores[1] = jogador3 == undefined ? 'jogador' : jogador3
           nomeJogadores[2] = jogador4 == undefined ? 'jogador' : jogador4
           nomeJogadores[3] = jogador1 == undefined ? 'jogador' : jogador1
+          
+          idJogadores[0] = id2 == undefined ? 'jogador' : id2
+          idJogadores[1] = id3 == undefined ? 'jogador' : id3
+          idJogadores[2] = id4 == undefined ? 'jogador' : id4
+          idJogadores[3] = id1 == undefined ? 'jogador' : id1
         } else if (props.numeroJogadores == 3) {
+          nomeJogadores[0] = jogador3 == undefined ? 'jogador' : jogador3
           nomeJogadores[1] = jogador4 == undefined ? 'jogador' : jogador4
           nomeJogadores[2] = jogador1 == undefined ? 'jogador' : jogador1
           nomeJogadores[3] = jogador2 == undefined ? 'jogador' : jogador2
+          
+          idJogadores[0] = id3 == undefined ? 'jogador' : id3
+          idJogadores[1] = id4 == undefined ? 'jogador' : id4
+          idJogadores[2] = id1 == undefined ? 'jogador' : id1
+          idJogadores[3] = id2 == undefined ? 'jogador' : id2
         } else if (props.numeroJogadores == 4) {
+          nomeJogadores[0] = jogador4 == undefined ? 'jogador' : jogador4
           nomeJogadores[1] = jogador1 == undefined ? 'jogador' : jogador1
           nomeJogadores[2] = jogador2 == undefined ? 'jogador' : jogador2
           nomeJogadores[3] = jogador3 == undefined ? 'jogador' : jogador3
+          
+          idJogadores[0] = id4 == undefined ? 'jogador' : id4
+          idJogadores[1] = id1 == undefined ? 'jogador' : id1
+          idJogadores[2] = id2 == undefined ? 'jogador' : id2
+          idJogadores[3] = id3 == undefined ? 'jogador' : id3
         }
 
         props.alteraOrdemJogadores(nomeJogadores)
+        props.alteraJogadoresIdPartida(idJogadores)
 
         if (controleJogador == 4) {
           props.alteraCheia(true)
@@ -97,19 +134,12 @@ const BodyLobby = props => {
         }
       }
 
-      // Simulação de atualização do atributo
-      // No seu caso, você pode fazer uma chamada API ou outra lógica
-      const newValue = false
-      setAttribute(newValue)
-
       if (attribute) {
         clearInterval(interval) // Para a rotina se o atributo mudar
         console.log('Atributo mudou, rotina parada')
       }
-    }, 1500) // Executa a rotina a cada segundo
+    }, 800) // Executa a rotina a cada segundo
   }
-
-  async function organiza() {}
 
   async function pegaUser(id) {
     const token = localStorage.getItem('token')
